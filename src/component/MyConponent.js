1e0442ce3c5108969636022bc4bf65a54b5fb2af
alysia <nguyenduclongbede@gmail.com>
2024-11-08 17:07:00 +0700

    commit lần 3

--- src/component/MyConponent.js
+++ src/component/MyConponent.js
@@ -1,11 +1,23 @@
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Long",
+    age: 2000,
+  };
+  handlerClick = (event) => {
+    this.setState({ name: "Dragon", age: Math.floor(Math.random() * 101) });
+  };
+  handlerMouseOver = (event) => {
+    console.log(event.pageX);
+  };
+
   //jsx
   render() {
     return (
       <div>
-        My first Component
-        {Math.floor(Math.random() * 101)};
+        My name is {this.state.name}. My age is {this.state.age}
+        <button onClick={this.handlerClick}>Click me!</button>
+        <button onMouseOver={this.handlerMouseOver}>Hover me!</button>
       </div>
     );
   }
