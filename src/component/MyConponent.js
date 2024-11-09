a249a5b665a3e3e52bbaabc5fc6fbd7acd29e3a7
alysia <nguyenduclongbede@gmail.com>
2024-11-09 14:41:02 +0700

    commit 8

--- src/component/MyConponent.js
+++ src/component/MyConponent.js
@@ -4,9 +4,9 @@ import MyInfor from "./MyInfor";
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "Dragon", age: 26 },
-      { id: 2, name: "Long", age: 24 },
-      { id: 3, name: "KingDragon", age: 30 },
+      { id: 1, name: "Dragon", age: "16" },
+      { id: 2, name: "Long", age: "24" },
+      { id: 3, name: "KingDragon", age: "30" },
     ],
   };
   //jsx
--- src/component/MyInfor.js
+++ src/component/MyInfor.js
@@ -1,18 +1,37 @@
 import React from "react";
 class MyInfor extends React.Component {
+  state = {
+    showListUsers: true,
+  };
+  handleShowHide = () => {
+    this.setState({ showListUsers: !this.state.showListUsers });
+  };
   render() {
     const { listUsers } = this.props;
     return (
       <div>
-        {listUsers.map((user) => {
-          console.log(user);
-          return (
-            <div key={user.id}>
-              <div>My name's {user.name}</div>
-              <div>My age is {user.age}</div>
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.showListUsers ? "Hide list users" : "Show list users"}
+          </span>
+        </div>
+        {this.state.showListUsers && (
+          <div>
+            {listUsers.map((user) => {
+              console.log(user);
+              return (
+                <div key={user.id} className={user.age < 18 ? "red" : "green"}>
+                  <div>My name's {user.name}</div>
+                  <div>My age is {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
